f79a651083ac357e882afd4dae684457013097af
Martin Preukschat <martin.preukschat@mdkgmbh.de>
2022-07-10 15:03:14 +0200

    ....

--- eiv_builder/resources/testAppConfig2.js
+++ eiv_builder/resources/testAppConfig2.js
@@ -1,5 +1,5 @@
 ccm.files['testAppConfig2.js']=  {
     "key": "demo",
-    "pdf": "https://mpreuk2s.github.io/mpreuk2s-components/eiv_builder/resources/slides.pdf"
+    "pdf": "https://mpreuk2s.github.io/mpreuk2s-components/eiv_builder/resources/slides.pdf",
     "page":2
 }
